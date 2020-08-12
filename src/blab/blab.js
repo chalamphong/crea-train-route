1ca250c6db946f1ca4f31e7743a382b605438757
Chalamphong Pandey <2201pandey@gmail.com>
2020-08-12 15:27:56 +0700

    Add skeleton for logger

---
+++ src/blab/blab.js
@@ -0,0 +1,13 @@
+const blab = {
+  info: text => {
+    console.log(text);
+  },
+  warn: text => {
+    console.log(text);
+  },
+  error: text => {
+    console.log(text);
+  }
+};
+
+export default blab;
